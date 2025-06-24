8778691cb4e42bb796370c7ecfe21995140f8b50
Subhash Kumar <skumar.iiita@gmail.com>
2025-06-24 10:50:43 +0530

    Daily DSA

---
+++ app/src/main/java/com/example/dsa_100/array/FindKDistantIndices.kt
@@ -0,0 +1,31 @@
+package com.example.dsa_100.array
+
+fun findKDistantIndices(nums: IntArray, key: Int, k: Int): List<Int> {
+    val result = mutableListOf<Int>()
+    for (i in nums.indices) {
+        if (nums[i] == key) {
+            val start = maxOf(0, i - k)
+            val end = minOf(nums.size - 1, i + k)
+            for (j in start..end) {
+                if (j !in result) {
+                    result.add(j)
+                }
+            }
+        }
+    }
+    return result
+}
+
+fun main() {
+    val alphabets = listOf("B", "a", "C")
+    val sortedList = alphabets.sorted()
+    println(sortedList)
+}
+
+//fun main() {
+//    val nums = intArrayOf(3, 4, 9, 1, 3, 9, 5)
+//    val key = 9
+//    val k = 1
+//    val result = findKDistantIndices(nums, key, k)
+//    println(result)
+//}
